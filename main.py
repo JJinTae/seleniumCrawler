24a5b887eb0dac44073810f996ffaf19b2429515
JJinTae <46085058+JJinTae@users.noreply.github.com>
2021-03-12 20:02:37 +0900

    기본적인 find_element 함수 사용 - 특정페이지의 제목, 날짜, 부서 등의 정보 스크랩 - selenium click()함수를 사용하여 이전 페이지로 이동가능
    ※ 한글뷰어의 내용(content)을 가져오는 방법 구현 중

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
+import selenium
+from selenium import webdriver
+# from selenium.webdriver import ActionChains
 
-def Test():
-    print('hello Sel')
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.by import By
+
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.support.ui import Select
+# from selenium.webdriver.support.ui import WebDriverWait
+
+
+
+def main():
+    URL = "https://www.mcst.go.kr/kor/s_notice/press/pressView.jsp?pSeq=17732"
+
+    options = webdriver.ChromeOptions()
+    options.add_argument('window-size=1920,1080')
+
+    driver = webdriver.Chrome('chromedriver', options=options)
+    driver.implicitly_wait(time_to_wait=3) # 암묵적 대기 단위 초
+    driver.get(url=URL)
+
+    post_title = driver.find_element_by_class_name('view_title')
+    post_date = driver.find_element_by_xpath('//*[@id="content"]/div[2]/div[2]/dl/dd[1]')
+    post_department = driver.find_element_by_xpath('//*[@id="content"]/div[2]/div[2]/dl/dd[3]')
+    post_content = driver.find_element_by_class_name('viewWarp')
+
+
+    # 이전 페이지로 넘어가는 방법
+    btn_prePage = driver.find_element_by_class_name('ico_arrow_top')
+    btn_prePage.click()
+
+
+    driver.close()
+
 
 
 if __name__ == '__main__':
-    Test()
+    main()
