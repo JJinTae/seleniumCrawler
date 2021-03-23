886dca970f1153229cc60cf8ce040642efea8835
JJinTae <46085058+JJinTae@users.noreply.github.com>
2021-03-24 02:42:08 +0900

    Test Scraping review - 2241개 데이터 수집 - 비어있는 데이터 확인(26/2241) ※ 비어있는 데이터 확인하기

--- scraping_Chungnam.py
+++ scraping_Chungnam.py
@@ -6,14 +6,13 @@ import requests
 import os
 import csv
 
+DATA_DIR = 'ScrapData'
+CSV_POST = os.path.join(DATA_DIR, 'post.csv')
 
 def main():
     URL = search_date(20200101, 20201231) # 수집할 보도자료 기간 ex) search_date(20200101, 20201231)
     list = get_list(URL)
     scrap_content(list)
-
-
-
 
 
 
@@ -37,9 +36,14 @@ def main():
 def scrap_content(list):
     driver2 = webdriver.Chrome('chromedriver', options=driver_option())
     driver2.implicitly_wait(time_to_wait=5)  # 암묵적 대기 단위 초
-
+    cols = ['date', 'department', 'title', 'content']
     count = 0
     flag = False
+
+    if not os.path.exists(CSV_POST):
+        with open(CSV_POST, 'w', newline='', encoding='utf-8') as f:
+            w = csv.writer(f)
+            w.writerow(cols)
 
     for post in list:
         if(flag):
@@ -50,12 +54,25 @@ def scrap_content(list):
             #print(i.get_attribute("href"))
             driver2.get(url=post.get_attribute("href"))
             time.sleep(1)
-            date = driver2.find_element_by_xpath("/html/body/form/table/tbody/tr[2]/td[2]/table/tbody/tr[3]/td/div[1]/table/tbody/tr[2]/td[1]").text
-            department = driver2.find_element_by_xpath("/html/body/form/table/tbody/tr[2]/td[2]/table/tbody/tr[3]/td/div[1]/table/tbody/tr[6]/td[1]").text
-            title = driver2.find_element_by_xpath("/html/body/form/table/tbody/tr[2]/td[2]/table/tbody/tr[3]/td/div[1]/table/tbody/tr[8]/td").text
-            content = driver2.find_element_by_xpath("/html/body/form/table/tbody/tr[2]/td[2]/table/tbody/tr[3]/td/div[1]/table/tbody/tr[16]/td").text
-            content = remove_whitespaces(content)
-            #print("DATE : " + date + "\nDEPARTMENT : " + department + "\nTITLE : " + title + "\nCONTENT :" + content)
+            try:
+                date = driver2.find_element_by_xpath(
+                    "/html/body/form/table/tbody/tr[2]/td[2]/table/tbody/tr[3]/td/div[1]/table/tbody/tr[2]/td[1]").text
+                department = driver2.find_element_by_xpath(
+                    "/html/body/form/table/tbody/tr[2]/td[2]/table/tbody/tr[3]/td/div[1]/table/tbody/tr[6]/td[1]").text
+                title = driver2.find_element_by_xpath(
+                    "/html/body/form/table/tbody/tr[2]/td[2]/table/tbody/tr[3]/td/div[1]/table/tbody/tr[8]/td").text
+                content = driver2.find_element_by_xpath(
+                    "/html/body/form/table/tbody/tr[2]/td[2]/table/tbody/tr[3]/td/div[1]/table/tbody/tr[16]/td").text
+                content = remove_whitespaces(content)
+                # print("DATE : " + date + "\nDEPARTMENT : " + department + "\nTITLE : " + title + "\nCONTENT :" + content)
+                row = [date, department, title, content]
+                with open(CSV_POST, 'a', newline='', encoding='utf-8') as f:
+                    w = csv.writer(f)
+                    w.writerow(row)
+            except:
+                print("데이터가 비었습니다.")
+
+
         else:
             print("통과되었습니다.")
             flag = True
@@ -65,6 +82,7 @@ def scrap_content(list):
 def driver_option():
     options = webdriver.ChromeOptions()
     options.add_argument('window-size=1920,1080')
+    options.add_argument('headless')
 
     return options
 
@@ -73,10 +91,10 @@ def get_list(url):
     driver.implicitly_wait(time_to_wait=5)  # 암묵적 대기 단위 초
     driver.get(url=url)
 
-    list = driver.find_element_by_css_selector("tbody").find_elements_by_tag_name("a")
-    print(len(list))
+    postList = driver.find_element_by_css_selector("tbody").find_elements_by_tag_name("a")
+    print(len(postList))
 
-    return list
+    return postList
 
 def read_html(url):
     headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"}
