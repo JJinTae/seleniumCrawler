d945587ae84c6c29807956eefe0b5c8945f35c02
JJinTae <46085058+JJinTae@users.noreply.github.com>
2021-05-19 11:34:27 +0900

    Add scraping_Locallaws.py(자치법규정보시스템 스크랩핑) - 자치법규정보시스템 서울시 제외 스크랩가능 - 특이한 오류 검출 X

--- scraping_Locallaws.py
+++ scraping_Locallaws.py
@@ -1,3 +1,4 @@
+import csv
 import os
 import time
 from selenium import webdriver
@@ -6,43 +7,78 @@ DATA_DIR = 'ScrapData'
 CSV_POST = os.path.join(DATA_DIR, 'localLaws.csv')
 
 def main():
-    URL = "http://www.elis.go.kr/newlaib/renew_laibLaws/h1126/laws_new.jsp?regionId=44000"
-    get_list(url=URL)
+    # listURL 해당지역 자치법규 목록 검색구분
+    listURL = "http://www.elis.go.kr/newlaib/renew_laibLaws/h1126/print_index_new.jsp?region=44000&gubun=2"
+    homeURL = "http://www.elis.go.kr/newlaib/renew_laibLaws/h1126/laws_new.jsp?regionId=44000"
 
+    cols = ['index', 'department', 'title', 'content']
+
+    if not os.path.exists(CSV_POST):
+        with open(CSV_POST, 'w', newline='', encoding='utf-8') as f:
+            w = csv.writer(f)
+            w.writerow(cols)
+
+    get_list(listURL, homeURL)
+
+
+
+def get_list(listURL, homeURL):
+    driver = webdriver.Chrome('chromedriver', options=driver_option())
+    driver.implicitly_wait(time_to_wait=10)  # 암묵적 대기 단위 초
+    driver.get(url=listURL)
+
+    driver2 = webdriver.Chrome('chromedriver', options=driver_option())
+    driver2.implicitly_wait(time_to_wait=10)
+    driver2.get(url=homeURL)
+    driver2.switch_to.frame("lawsif")
+
+    index = 1
+    linkList = driver.find_element_by_css_selector("tbody").find_elements_by_class_name("title")
+
+    for i in linkList:
+        link = i.find_element_by_tag_name("a").get_attribute("href").replace("javascript:goViewlaws", "goLaws")
+        print(index) # index
+        print(i.find_element_by_xpath("following-sibling::td").text) # department
+        print(i.text)  # title
+        print(i.find_element_by_tag_name("a").get_attribute("href").replace("javascript:goViewlaws", "goLaws")) # link
+        print(remove_whitespaces(get_content(driver2, link)))  # content
+
+        department = i.find_element_by_xpath("following-sibling::td").text
+        title = i.text
+        content = remove_whitespaces(get_content(driver2, link))
+
+        row = [index, department, title, content]
+        with open(CSV_POST, 'a', newline='', encoding='utf-8') as f:
+            w = csv.writer(f)
+            w.writerow(row)
+
+        index = index + 1
+
+
+
+def get_content(driver, link):
+
+    driver.switch_to.frame("left")
+    driver.switch_to.frame("index2if")
+    driver.execute_script(link)
+    time.sleep(1)
+    driver.switch_to.parent_frame()
+    driver.switch_to.parent_frame()
+    driver.switch_to.frame("right")
+    content = driver.find_element_by_class_name("rightview").text
+    driver.switch_to.parent_frame()
+
+    return content
 
 
 
 def driver_option():
     options = webdriver.ChromeOptions()
     options.add_argument('window-size=1920,1080')
-    # options.add_argument('headless')
+    options.add_argument('headless')
 
     return options
 
-
-def get_list(url):
-    driver = webdriver.Chrome('chromedriver', options=driver_option())
-    driver.implicitly_wait(time_to_wait=10)  # 암묵적 대기 단위 초
-    driver.get(url=url)
-
-    listHtml = []
-    # postList = driver.find_element_by_css_selector("tbody").find_elements_by_tag_name("a")
-    # postList = driver.find_element_by_css_selector("div.raw_sort")
-    driver.switch_to.frame("lawsif")
-    driver.switch_to.frame("left")
-    driver.switch_to.frame("index2if")
-    postList = driver.find_element_by_css_selector("div.raw_sort").find_elements_by_css_selector("ul.tree3")
-    for i in postList:
-        for html in i.find_elements_by_tag_name("a"):
-            listHtml.append(html.get_attribute("html"))
-
-
-
-
-
-
-    time.sleep(10)
-    # return postList
 
 
 def remove_whitespaces(text):
